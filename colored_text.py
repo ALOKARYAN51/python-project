8aff44c6ac9d6122339a33ca21725fff23018093
ALOK KUMAR <56088808+ALOKARYAN51@users.noreply.github.com>
2021-06-04 23:03:43 +0530

    Create colored_text.py

---
+++ colored_text.py
@@ -0,0 +1,8 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset = True)
+
+print(Fore.BLUE+Back.YELLOW+"Hello I am Alok Aryan.")
+print(Back.CYAN+"Hi My name is Alok Aryan. I am web developer enthusiast")
+print(Back.RED+"Hi My name is Alok Aryan. I am data scientist")
+print(Back.GREEN+"Hi My name is Alok Aryan. I want to be SDE 1 intern at Amazone")
